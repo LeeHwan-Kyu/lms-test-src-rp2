29243695ef91e6cb0138069be52ce3dd6f275ff4
l-hwangyu <rlxk2279@gmail.com>
2025-11-20 17:48:06 +0900

    add: Case02 ログイン認証失敗テストを追加
    未登録ユーザーでログインした際にエラーメッセージが表示されることを確認するテストを作成。
    トップページへのアクセス、ID/パスワード入力、ログインボタン押下までの一連の流れを自動化し、
    エラー表示の有無を明確に検証できるようにした。
    
    ・入力値：不正なログインID／パスワード
    ・期待値：エラーメッセージが表示されること
    ・エビデンス：Case02_test01 / Case02_test02 を取得

--- test_code_creation_leehwangyu/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
+++ test_code_creation_leehwangyu/src/test/java/jp/co/sss/lms/ct/f01_login1/Case02.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +12,10 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト ログイン機能①
@@ -35,14 +42,47 @@ public class Case02 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページURLにアクセス
+		webDriver.get("http://localhost:8080/lms");
+
+		//タイトル「ログイン | LMS」になるまで待機
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+		wait.until(ExpectedConditions.titleIs("ログイン | LMS"));
+
+		//タイトルチェック
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//Case02_test01エビデンス取得
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 DBに登録されていないユーザーでログイン")
-	void test02() {
-		// TODO ここに追加
-	}
+	void test02() throws InterruptedException {
 
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+
+		//不正なログインIDとパスワードを入力
+		webDriver.findElement(By.id("loginId")).sendKeys("Case02TestId");
+		webDriver.findElement(By.id("password")).sendKeys("Case02TestPW");
+
+		//ログインボタン押下
+		webDriver.findElement(
+				By.cssSelector("input[type='submit'][value='ログイン']")).click();
+
+		//エラーメッセージの表示を待機
+		WebElement errorMessage = wait.until(
+				ExpectedConditions.presenceOfElementLocated(
+						By.cssSelector("span.error")));
+
+		//エラーメッセージが表示されていることを確認
+		assertTrue(errorMessage.getText().contains("ログイン"),
+				"エラーメッセージが表示されていません");
+
+		//Case02_test02エビデンス取得
+		getEvidence(new Object() {
+		});
+	}
 }
