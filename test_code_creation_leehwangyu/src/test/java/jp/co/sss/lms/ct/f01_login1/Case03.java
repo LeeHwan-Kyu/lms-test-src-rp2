07490e474cf5c67ff0f941e8187281e578d82a09
l-hwangyu <rlxk2279@gmail.com>
2025-11-21 17:28:55 +0900

    add: ケース03 受講生ログイン正常系のテストコード追加
    update: 結合テスト仕様書（Case03）を更新

--- test_code_creation_leehwangyu/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
+++ test_code_creation_leehwangyu/src/test/java/jp/co/sss/lms/ct/f01_login1/Case03.java
@@ -1,6 +1,9 @@
 package jp.co.sss.lms.ct.f01_login1;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.Duration;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,23 +12,23 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * 結合テスト ログイン機能①
  * ケース03
- * @author holy
  */
 @TestMethodOrder(OrderAnnotation.class)
 @DisplayName("ケース03 受講生 ログイン 正常系")
 public class Case03 {
 
-	/** 前処理 */
 	@BeforeAll
 	static void before() {
 		createDriver();
 	}
 
-	/** 後処理 */
 	@AfterAll
 	static void after() {
 		closeDriver();
@@ -35,14 +38,50 @@ public class Case03 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		//トップページURLにアクセス
+		webDriver.get("http://localhost:8080/lms");
+
+		//タイトル「ログイン | LMS」になるまで待機
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+		wait.until(ExpectedConditions.titleIs("ログイン | LMS"));
+
+		//タイトルチェック
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+
+		//エビデンス
+		getEvidence(new Object() {
+		});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
-	}
 
+		WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+
+		//環境変数からID/PW取得
+		String id = System.getenv("LMS_ID");
+		String pw = System.getenv("LMS_PW");
+
+
+		//ID/PW入力
+		webDriver.findElement(By.id("loginId")).sendKeys(id);
+		webDriver.findElement(By.id("password")).sendKeys(pw);
+
+		//ログインボタン押下
+		webDriver.findElement(
+				By.cssSelector("input[type='submit'][value='ログイン']")).click();
+
+		//遷移先が /course/detail になるまで待機
+		wait.until(ExpectedConditions.urlContains("/course/detail"));
+
+		//「コース詳細」という文字が表示されていることを確認
+		wait.until(ExpectedConditions.presenceOfElementLocated(
+				By.xpath("//*[contains(text(),'コース詳細')]")));
+
+		//エビデンス
+		getEvidence(new Object() {
+		});
+	}
 }
